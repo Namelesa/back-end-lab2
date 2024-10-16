2dd20c0df833c3f2ce810752c35d9d80f2cd8df8
maksimbilyk <pdo090318@gmail.com>
2024-10-16 10:38:57 +0300

    Add Controllers

--- RESTAPI/Controllers/CategoryController.cs
+++ RESTAPI/Controllers/CategoryController.cs
@@ -1,6 +1,58 @@
+using Microsoft.AspNetCore.Mvc;
+using RESTAPI.Models;
+
 namespace RESTAPI.Controllers;
 
-public class CategoryController
+[Route("[controller]")]
+[ApiController]
+public class CategoryController : ControllerBase
 {
     
+    private static List<Category> categories = new List<Category>
+    {
+        new Category { Id = 1, CategoryName = "CategoryTest1"},
+        new Category { Id = 2, CategoryName = "CategoryTest2"}
+    };
+
+    
+    [HttpGet("/category")]
+    public ActionResult<Category> GetCategory(string categoryName)
+    {
+        var category = categories.FirstOrDefault(p => p.CategoryName == categoryName);
+        if (category == null)
+        {
+            return NotFound();
+        }
+        return Ok(category);
+    }
+    
+    [HttpPost("/category")]
+    public IActionResult AddCategory(string categoryName)
+    {
+        var currentCategory = categories.FirstOrDefault(p => p.CategoryName == categoryName);
+        if (currentCategory == null)
+        {
+            Category category = new Category()
+            {
+                CategoryName = categoryName,
+            };
+            category.Id = categories.Max(p => p.Id) + 1;
+            categories.Add(category);
+            return Ok(category);
+        }
+        
+        return BadRequest("You already have this category");
+    }
+    
+    [HttpDelete("/category")]
+    public IActionResult DeleteCategory(string categoryName)
+    {
+        var category = categories.FirstOrDefault(p => p.CategoryName == categoryName);
+        if (category == null)
+        {
+            return NotFound();
+        }
+        categories.Remove(category);
+        return Ok("You delete this category");
+    }
 }
--- RESTAPI/Controllers/RecordController.cs
+++ RESTAPI/Controllers/RecordController.cs
@@ -1,6 +1,84 @@
+using Microsoft.AspNetCore.Mvc;
+using RESTAPI.Models;
+
 namespace RESTAPI.Controllers;
 
-public class RecordController
+[Route("[controller]")]
+[ApiController]
+public class RecordController: ControllerBase
 {
+    private static List<Record> records = new List<Record>
+    {
+        new Record { Id = 1, UserId = 1, CategoryId = 1, Date = DateTime.Now, TotalPrice = 1},
+        new Record { Id = 2, UserId = 3, CategoryId = 1, Date = DateTime.Now, TotalPrice = 2},
+        new Record { Id = 3, UserId = 2, CategoryId = 2, Date = DateTime.Now, TotalPrice = 3},
+        new Record { Id = 4, UserId = 2, CategoryId = 3, Date = DateTime.Now, TotalPrice = 4}
+    };
     
+    [HttpGet("/record/{record_id}")]
+    public ActionResult<Record> GetRecordById(int record_id)
+    {
+        var record = records.FirstOrDefault(p => p.Id == record_id);
+        if (record == null)
+        {
+            return NotFound();
+        }
+        return Ok(record);
+    }
+    
+    [HttpDelete("/record/{record_id}")]
+    public IActionResult DeleteRecordById(int record_id)
+    {
+        var record = records.FirstOrDefault(p => p.Id == record_id);
+        if (record == null)
+        {
+            return NotFound();
+        }
+        records.Remove(record);
+        return Ok("You delete this record");
+    }
+    
+    [HttpPost("/record")]
+    public IActionResult AddRecord(int userId, int categoryId, decimal total)
+    {
+        Record record = new Record()
+        {
+            UserId = userId,
+            CategoryId = categoryId,
+            Date = DateTime.Now,
+            TotalPrice = total
+        };
+        record.Id = records.Max(p => p.Id) + 1;
+        records.Add(record);
+        return CreatedAtAction(nameof(GetRecordById), new { id = record.Id }, record);
+    }
+    
+    [HttpGet("/record")]
+    public ActionResult GetRecord(int? userId, int? categoryId)
+    {
+        if (userId == null && categoryId == null)
+        {
+            return BadRequest("You don't write a params");
+        }
+        
+        var filteredRecords = records.AsQueryable();
+
+        if (userId != null)
+        {
+            filteredRecords = filteredRecords.Where(p => p.UserId == userId);
+        }
+
+        if (categoryId != null)
+        {
+            filteredRecords = filteredRecords.Where(p => p.CategoryId == categoryId);
+        }
+        
+        if (!filteredRecords.Any())
+        {
+            return NotFound("No records found with the specified criteria.");
+        }
+
+        return Ok(filteredRecords);
+    }
+
 }
